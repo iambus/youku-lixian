80587071067ba211fc3afc471d256f9a95a68822
Boyu Guo <iambus@gmail.com>
2012-09-16 13:11:07 +0800

    update bilibili

--- bilibili.py
+++ bilibili.py
@@ -9,7 +9,7 @@ from youku import youku_download_by_id
 from tudou import tudou_download_by_id
 
 def get_srt_xml(id):
-	url = 'http://comment.bilibili.tv/dm,%s' % id
+	url = 'http://comment.bilibili.tv/%s.xml' % id
 	return get_html(url).decode('utf-8')
 
 def parse_srt_p(p):
@@ -45,6 +45,18 @@ def parse_srt_xml(xml):
 		p = parse_srt_p(x)
 	raise NotImplementedError()
 
+def parse_cid_playurl(xml):
+	from xml.dom.minidom import parseString
+	doc = parseString(xml.encode('utf-8'))
+	urls = [durl.getElementsByTagName('url')[0].firstChild.nodeValue for durl in doc.getElementsByTagName('durl')]
+	return urls
+
+def bilibili_download_by_cid(id, title, merge=True):
+	url = 'http://interface.bilibili.tv/playurl?cid=' + id
+	urls = parse_cid_playurl(get_html(url, 'utf-8'))
+	assert re.search(r'\.(flv|hlv)\b', urls[0]), urls[0]
+	download_urls(urls, title, 'flv', total_size=None, merge=merge)
+
 def bilibili_download(url, merge=True):
 	assert re.match(r'http://(www.bilibili.tv|bilibili.kankanews.com)/video/av(\d+)', url)
 	html = get_html(url)
@@ -53,10 +65,12 @@ def bilibili_download(url, merge=True):
 	title = unescape_html(title)
 	title = escape_file_path(title)
 
-	flashvars = r1(r'flashvars="([^"]+)"', html)
+	flashvars = r1_of([r'flashvars="([^"]+)"', r'"https://secure.bilibili.tv/secure,(cid=\d+)"'], html)
 	assert flashvars
 	t, id = flashvars.split('=', 1)
-	if t == 'vid':
+	if t == 'cid':
+		bilibili_download_by_cid(id, title, merge=merge)
+	elif t == 'vid':
 		iask_download_by_id(id, title, merge=merge)
 	elif t == 'ykid':
 		youku_download_by_id(id, title, merge=merge)
